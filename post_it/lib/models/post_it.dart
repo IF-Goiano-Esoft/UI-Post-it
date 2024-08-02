a4ee68c69c3fa933f2a097af43bafb6f6bf83242
ArthurLVidal <arthurodri2017@gmail.com>
2024-08-02 16:12:56 -0300

    Implementando lógica e botão para excluir Post-It e adicionando Id para classe Post It, para identificar o id de cada Post It

--- post_it/lib/models/post_it.dart
+++ post_it/lib/models/post_it.dart
@@ -1,15 +1,19 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class PostIt {
+  final String id;
   final String title;
   final String description;
   final DateTime date;
   final Color color;
 
   PostIt({
+    String? id,
     required this.title,
     required this.description,
     required this.date,
     this.color = const Color(0xfffFEB3B),
-  });
+  }) : id = id ?? Random().nextDouble().toString();
 }
