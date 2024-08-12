e8a168f286ddb36ba5b5b72018511eca1c22be15
ArthurLVidal <arthurodri2017@gmail.com>
2024-08-12 08:09:30 -0300

    Correção de formulario

--- post_it/lib/screens/new_post_it_screen/components/form_field_widget.dart
+++ post_it/lib/screens/new_post_it_screen/components/form_field_widget.dart
@@ -33,103 +33,104 @@ class _FormFieldWidgetState extends State<FormFieldWidget> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        children: [
-          // Text Field Titulo
-          TextFormField(
-            controller: _titleController,
-            validator: (value) => titleValidator(value),
-            decoration: const InputDecoration(
-              icon: Icon(Icons.title),
-              label: Text('Titulo'),
-              hintText: 'Pergunta',
-            ),
-          ),
-          const SizedBox(height: 25),
-
-          // Text Field Description
-          TextFormField(
-            controller: _descriptionController,
-            validator: (value) => descriptionValidator(value),
-            decoration: const InputDecoration(
-              icon: Icon(Icons.question_answer),
-              label: Text('Descrição'),
-              hintText: 'Resposta',
-            ),
-          ),
-          const SizedBox(height: 25),
-
-          // Selecionar Data
-          Row(
-            children: [
-              const SizedBox(
-                width: 150,
-                child: IconSelectData(),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Text Field Titulo
+            TextFormField(
+              controller: _titleController,
+              validator: (value) => titleValidator(value),
+              decoration: const InputDecoration(
+                icon: Icon(Icons.title),
+                label: Text('Titulo'),
+                hintText: 'Pergunta',
               ),
-              DatePicker(context),
-            ],
-          ),
-          const SizedBox(height: 25),
+            ),
+            const SizedBox(height: 25),
 
-          // Dropdown escolha de Cores
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.color_lens_outlined,
-                    size: 29,
-                    color: Colors.grey.shade800,
-                  ),
-                  const SizedBox(width: 8),
-                  const Text(
-                    'Escolha a cor do Post It: ',
-                    style: TextStyle(color: Color.fromARGB(255, 109, 109, 109)),
-                  ),
-                ],
+            // Text Field Description
+            TextFormField(
+              controller: _descriptionController,
+              validator: (value) => descriptionValidator(value),
+              decoration: const InputDecoration(
+                icon: Icon(Icons.question_answer),
+                label: Text('Descrição'),
+                hintText: 'Resposta',
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: DropdownButton(
-                  value: selectedColor,
-                  elevation: 16,
-                  underline: Container(
-                    height: 2,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  items:
-                      listColor.keys.map<DropdownMenuItem<String>>((nameColor) {
-                    return DropdownMenuItem(
-                      value: nameColor,
-                      child: Text(nameColor),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      selectedColor = value!;
-                    });
-                  },
+            ),
+            const SizedBox(height: 25),
+
+            // Selecionar Data
+            Row(
+              children: [
+                const SizedBox(
+                  width: 150,
+                  child: IconSelectData(),
                 ),
-              ),
-            ],
-          ),
+                DatePicker(context),
+              ],
+            ),
+            const SizedBox(height: 25),
 
-          const Expanded(child: SizedBox()),
+            // Dropdown escolha de Cores
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.color_lens_outlined,
+                      size: 29,
+                      color: Colors.grey.shade800,
+                    ),
+                    const SizedBox(width: 8),
+                    const Text(
+                      'Escolha a cor do Post It: ',
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 109, 109, 109)),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButton(
+                    value: selectedColor,
+                    elevation: 16,
+                    underline: Container(
+                      height: 2,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    items: listColor.keys
+                        .map<DropdownMenuItem<String>>((nameColor) {
+                      return DropdownMenuItem(
+                        value: nameColor,
+                        child: Text(nameColor),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedColor = value!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
 
-          // Botões para enviar e cancelar
-          RowButtonSendCancel(
-            selectedDate: _selectedDate,
-            selectedColor: listColor[selectedColor],
-            formKey: _formKey,
-            titleController: _titleController,
-            descriptionController: _descriptionController,
-            onPostItAdded: (postIt) {
-              Navigator.of(context).pop(postIt);
-            },
-            user: AuthService().currentUser!,
-          ),
-        ],
+            // Botões para enviar e cancelar
+            RowButtonSendCancel(
+              selectedDate: _selectedDate,
+              selectedColor: listColor[selectedColor],
+              formKey: _formKey,
+              titleController: _titleController,
+              descriptionController: _descriptionController,
+              onPostItAdded: (postIt) {
+                Navigator.of(context).pop(postIt);
+              },
+              user: AuthService().currentUser!,
+            ),
+          ],
+        ),
       ),
     );
   }
